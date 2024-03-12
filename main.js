e0591bdaeecabc4022ad2889992fe2556231dfcb
LauraKoda <162718630+LauraKoda@users.noreply.github.com>
2024-03-12 11:23:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdfI9YKdv8rgS2FfkchC5I6bhRcufqGcR6vGCJ9ct_dKvt8qLwrJK4KQy3eWVKV71-8og&usqp=CAU://midias.correiobraziliense.com.br/_midias/jpg/2016/04/05/675x450/1_cbnfot050420160102-21057794.jpg?20220217210708?20220217210708"
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOvjSHYtIaiy8G_MUaXOVpzxx_kp-VmEIb2aTZDQUh5ZORteqnpPWIbMEYSGZPbvME_mc&usqp=CAU"
+}
+
+ 
